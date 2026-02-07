7ad4f4465bf66293a4d99e4a148d17c1e4f2ed1e
Santeri Korpela <143890623+sjkorpela@users.noreply.github.com>
2026-02-07 13:45:19 +0200

    Start work on drawing shapes

---
+++ godot-project/Assets/Scripts/AttackShape.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class AttackShape : Line2D
+{
+
+	private double _age = 0f;
+
+	private double _maxAge = 1f;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_age > _maxAge)
+		{
+			this.QueueFree();
+		}
+		_age += delta;
+	}
+}
